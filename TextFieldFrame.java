a4163643e13815d3b33b7c7c6af4aa7dde724ec1
jefferton000 <jefferton@gmail.com>
2015-07-27 20:43:26 -0700

    Update TextFieldFrame.java

--- TextFieldFrame.java
+++ TextFieldFrame.java
@@ -18,4 +18,27 @@ public class TextFieldFrame extends JFrame
   public TextFieldFrame()
   {
     super("Testing JTextField and JPasswordField");
-    setLayout(new FlowLayour());
+    setLayout(new FlowLayout());
+    
+    // construct textfield with 10 columns
+    textField1 = new JTextField(10);
+    add(textField1);  // add textField1 to JFrame
+    
+    // construct textfield with default text
+    textField2 = new JTextField("Enter text here");
+    add(textField2);  // add textField2 to JFrame
+    
+    // construct textfield with default text and 21 columns
+    textField3 = new JTextField("Uneditable text field", 21);
+    textField3.setEditable(false);  // disable editing
+    add(textField3);  // add textField3 to JFrame
+    
+    // construct passwordField with default text
+    passwordField = new JPasswordField("Hidden text");
+    add(passwordField);  // add passwordField to JFrame
+    
+    // register event handlers
+    TextFieldHandler handler = new TextFieldHandler();
+    textField1.addActionListener(handler);
+    textField1.addActionListener(handler);
+    textField1.addActionListener(handler);
