3a9025ea6b8f508c44fe56d0e40a461d40e6a55c
jefferton000 <jefferton@gmail.com>
2015-07-27 22:22:52 -0700

    Update ButtonFrame.java

--- ButtonFrame.java
+++ ButtonFrame.java
@@ -2,4 +2,47 @@
 import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+public class ButtonFrame extend JFrame
+{
+  private final JButton plainJButton; // button with just text
+  private final JButton fancyJButton; // button with icons
+  
+  // ButtonFrame adds JBuuton to JFrame
+  public ButtonFrame()
+  {
+    super("Testing Buttons");
+    setLayout(new FlowLayout());
+    
+    plainJButton = new JButton("Plain Button"); //button with text
+    add(plainJButton);  //add plainJButton to JFrame
+    
+    Icon bug1 = new ImageIcon(getClass().getResource("bug1.gif"));
+    Icon bug2 = new ImageIcon(getClass().getResource("bug2.gif"));
+    fancyJButton = new JButton("Fancy Button", bug1); //set image
+    fancyJButton.setRolloverIcon(bug2); /set rollover image
+    add(fancyJButton);  //add fancyJButton to JFrame
+    
+    // create new ButtonHandler for button event handling
+    ButtonHandler handler = new ButtonHandler();
+    fancyJButton.addActionListener(handler);
+    plainJButton.addActionListener(handler);
+  }
+  
+  // inner class for button event handling
+  private class ButtonHandler implements ActionListener
+  {
+    // handle button event
+    @Override
+    public void actionPerformed(ActionEvent event)
+    {
+      JOptionPane.showMessageDialog(ButtonFrame.this, String.format(
+        "You pressed: %s", event.getActionCommand()));
+    }
+  }
+}
