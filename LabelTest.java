34907b97209df246bd422edd3539fb8be3348fe0
jefferton000 <jefferton@gmail.com>
2015-07-27 19:42:06 -0700

    Create LabelTest.java

---
+++ LabelTest.java
@@ -0,0 +1,10 @@
+// Testing LabelFrame
+import javax.swing.JFrame;
+
+public class LabelTest
+{
+  public static void main(String[] args)
+  {
+    LabelFrame labelframe = new LabelFrame();
+    labelframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    labelframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
