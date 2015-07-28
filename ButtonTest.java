0a7f152ef5625fc9fdca22f7854a061c3506088a
jefferton000 <jefferton@gmail.com>
2015-07-27 21:46:53 -0700

    Create ButtonTest.java

---
+++ ButtonTest.java
@@ -0,0 +1,13 @@
+// Testing ButtonFrame
+import javax.swing.JFrame;
+
+public class ButtonTest
+{
+  public static void main(String[] args)
+  {
+    ButtonFrame buttonframe = new ButtonFrame();
+    buttonframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    buttonframe.setSize(275, 110);
+    buttonframe.setVisible(true);
+  }
+}
