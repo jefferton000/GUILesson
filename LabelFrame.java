1b361d0b25a45b7c9a4d05d903e1a4c8462fd7fc
jefferton000 <jefferton@gmail.com>
2015-07-27 20:10:04 -0700

    Update LabelFrame.java

--- LabelFrame.java
+++ LabelFrame.java
@@ -20,5 +20,22 @@ public class LabelFrame extends JFrame
     
     // JLabel constructor with a string argument
     label1 = new JLabel("Label with text");
+    label1.setToolTipText("This is label1");
+    add(label1);  // add label1 to JFrame
+    
+    // JLabel constructor with string, Icon, and alignment arguments
+    Icon bug = new ImageIcon(getClass().getResource("bug1.png"));
+    label2 = new JLabel("Label with text and icon", bug,
+      SwingConstants.LEFT);
+    label2.setToolTipText("This is label2");
+    add(label2);  // add label2 to JFrame
+    
+    label3 = new JLabel();  // JLabel constructor no arguments
+    label3.setText("Label with icon and text at bottom");
+    label3.setIcon(bug);  // add icon to JLabel
+    label3.setHorizontalTextPosition(SwingConstants.CENTER);
+    label3.setVerticalTextPosition(SwingConstants.BOTTOM);
+    label3.setToolTipText("This is label3");
+    add(label3);  // add label3 to JFrame
   }
 }
