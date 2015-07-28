1594c857bbe18e1b34268dfccf1f7c3069e75a55
jefferton000 <jefferton@gmail.com>
2015-07-27 19:26:00 -0700

    Create LabelFrame.java

---
+++ LabelFrame.java
@@ -0,0 +1,5 @@
+// JLabels with text and icons.
+import java.awt.FlowLayout;   // specifies how components are arranged
+import java.swing.JFrame;     // provides basic window features
+import java.swing.JFrame;     
+import java.swing.JFrame;     
